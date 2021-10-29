913a560df6c0ce7586e83973fa52eee02efa2a0a
monstrillllo <evgeniy27112000@gmail.com>
2021-10-29 19:53:25 +0300

    jhdksfl;

--- searcher.py
+++ searcher.py
@@ -1,12 +1,15 @@
 import math
 import os
+import sys
+import numpy as np
 from File import File
+import socket
 
 
 def get_folders_content() -> list:
     files_path_list = []
 
-    for path, _, filenames in os.walk('share2'):
+    for path, _, filenames in os.walk('share'):
         for file in filenames:
             if file.endswith('.txt'):
                 files_path_list.append(os.path.join(path, file))
@@ -55,7 +58,37 @@ def sort_key(file):
     return file.eq_rate
 
 
-def main(path: str, search: str):
-    files = get_files_list(get_folders_content())
-    eq_rating(files, search)
-    return sorted(files, key=sort_key, reverse=True)
+def start_socket():
+    sock = socket.socket()
+    sock.bind(('', 9099))
+    sock.listen(5)
+    conn, addr = sock.accept()
+    print(f'Connected: {addr}')
+    search_size = 0
+    while True:
+        if search_size == 0:
+            size_recv = conn.recv(8)
+            if size_recv:
+                search_size = int.from_bytes(size_recv, 'little')
+                print(search_size)
+        else:
+            search = conn.recv(search_size)
+            if search:
+                files = get_files_list(get_folders_content())
+                eq_rating(files, search.decode(encoding='utf-8'))
+                result = [[file.path, file.eq_rate] for file in sorted(files, key=sort_key, reverse=True)]
+                size = sys.getsizeof(np.array(result))
+                size_bytes = size.to_bytes(2, 'little')
+                conn.send(size_bytes)
+                conn.send(np.array(result).tobytes())
+                print(np.array(result))
+            search_size = 0
+            # break
+
+
+def main():
+    start_socket()
+
+
+if __name__ == '__main__':
+    main()
--- window.py
+++ window.py
@@ -1,7 +1,11 @@
+import socket
+import sys
+import json
 import mainWindow
 from PyQt5 import QtWidgets
 import searcher
 from Help import Help
+from AddIP import AddIP
 
 
 class SearchWindow(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
@@ -9,26 +13,38 @@ class SearchWindow(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.btn_search.clicked.connect(self.btn_search_clicked)
+        with open('./local_ips.json') as file:
+            self.comboBox_ip.addItems(json.load(file).get('ips'))
         self.folder_path = ''
         self.search_string = ''
         self.actionhelp.triggered.connect(self.help_clicked)
+        self.btn_add_ip.clicked.connect(self.add_clicked)
 
     def btn_search_clicked(self):
-        if not self.textEdit_search.toPlainText():
-            return
-        self.folder_path = self.textEdit_folder.toPlainText().strip().lower()
-        self.search_string = self.textEdit_search.toPlainText().strip().lower()
-        result = searcher.main(self.folder_path, self.search_string)
-        for file in result:
-            if file.eq_rate == 0:
-                break
-            self.plainTextEdit_result.insertPlainText(f'file path: {file.path}\nrating: {file.eq_rate}\n'
-                                                      f'words from search string: {file.words_from_search}\n*****\n')
+        try:
+            sock = socket.socket()
+            sock.connect((self.comboBox_ip.currentText(), 9099))
+            search_str = self.textEdit_search.toPlainText()
+            size = sys.getsizeof(search_str)
+            print(size)
+            size_bytes = bytes([size])
+            sock.send(size_bytes)
+            sock.send(bytes(search_str, 'utf-8'))
+            size = int.from_bytes(sock.recv(8), 'little')
+            data = sock.recv(size)
+            print(data.decode(encoding='utf-8'))
+            sock.close()
+        except Exception as e:
+            self.plainTextEdit_result.insertPlainText(f'cant connect to: {self.comboBox_ip.currentText()}\n*****\n')
+            print(e)
 
     def help_clicked(self):
         dial = Help(self)
-        # dial.setupUi(dial)
         dial.show()
+
+    def add_clicked(self):
+        add_dial = AddIP(self)
+        add_dial.show()
 
 
 if __name__ == '__main__':
